f96550237af05a601781a6e3df364f05170c7f68
Rocco Panacci <rocco@clausehound.com>
2017-10-09 21:12:15 -0400

    final

--- js/current.js
+++ js/current.js
@@ -1,10 +1,41 @@
 // The API url and the city we want to check...
 var url = "http://api.openweathermap.org/data/2.5/weather?callback=?";
 
-// Go call the URL and pass 2 parameters (q=city, units=metric/imperial)
-$.getJSON(url, {}, function(data)
-{
+var getTemp = function() {
 
-	console.log(data);
+	var city = $('#loc').val();
 
-});
+	var params = {
+		'q':city,
+		'units':'metric',
+		'APPID':'6dec5fb891e6e243c9d8c20351998e67'
+	}
+
+	// Go call the URL and pass 2 parameters (q=city, units=metric/imperial)
+	$.getJSON(url, params, function(result) {
+		// "Callback" function runs once the data is returned
+
+
+		// Data returns as a JSON object (we called it "data")
+		console.log(result);
+		// console.log("The temp is " + result.main.temp);
+
+		$('.curr span').html(result.main.temp)
+		$('.high span').html(result.main.temp_max)
+		$('.low span').html(result.main.temp_min)
+
+		if (result.main.temp > 0) {
+			var red = Math.round(result.main.temp * 10)
+			$('body').css('background-color', 'rgb('+red+', 0, 0)')
+		}
+		else {
+			var blue = Math.round(result.main.temp * -10)
+			$('body').css('background-color', 'rgb(0, 0, '+ (blue) +')')
+		}
+
+	});
+
+
+}
+
+$('#go').click(getTemp)
